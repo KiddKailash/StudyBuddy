72d7e8a6d359bf343afb7aab91d707faa333a6d1
Kailash Kidd <kailashkidd@Kailashs-MBP.modem>
2024-11-11 17:47:44 +1000

    Connected to correct Db cluster (test -> clipcard)

--- utils/db.js
+++ utils/db.js
@@ -1,14 +1,11 @@
-// utils/db.js
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
 const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri, {
-  tls: true, // Enable TLS
-  tlsAllowInvalidCertificates: false, // Set to true if using self-signed certificates (not recommended)
-  // You can also specify tlsCAFile if you have a specific CA certificate
-  // tlsCAFile: '/path/to/ca.pem',
+  tls: true,
+  tlsAllowInvalidCertificates: false,
 });
 
 let db;
@@ -17,8 +14,8 @@ const connectDB = async () => {
   if (db) return db;
   try {
     await client.connect();
-    db = client.db(); // Use the default database specified in the URI
-    console.log('Connected to MongoDB');
+    db = client.db('clipcard'); // Specify the 'clipcard' database here
+    console.log('Connected to MongoDB using database:', db.databaseName);
     return db;
   } catch (error) {
     console.error('MongoDB Connection Error:', error);
