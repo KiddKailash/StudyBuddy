b4f22d3e1bce515eed8ecac1666fb2a14dff2a53
Kailash Kidd <kailashkidd@Kailashs-MacBook-Pro.local>
2024-11-12 13:35:36 +1000

    Fixed minor typo which was blocking deploy

--- src/webpages/FlashcardSession.jsx
+++ src/webpages/FlashcardSession.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-import Flashcard from "../components/FlashCard";
+import Flashcard from "../components/Flashcard";
 
 // ================================
 // MUI Component Imports
