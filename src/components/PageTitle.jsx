5ad990a4c78883e5abd1749da10623f1400dd88d
Kailash Kidd <kiddkailash@gmail.com>
2024-11-27 22:43:39 +1000

    Implemented Translation

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -1,20 +1,27 @@
 import PropTypes from "prop-types";
+import React from "react";
 
 // MUI Component Imports
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid2";
 import Divider from "@mui/material/Divider";
 
+// Import the useTranslation hook
+import { useTranslation } from "react-i18next";
+
 /**
  * PageTitle component renders a page title with an optional subtitle.
  *
  * @param {Object} props - The component props.
- * @param {string} props.title - The main title text to display (required).
- * @param {string} [props.subtitle] - Optional subtitle text to display under the title.
+ * @param {string} props.titleKey - The translation key for the main title text to display (required).
+ * @param {string} [props.subtitleKey] - Optional translation key for subtitle text to display under the title.
  *
  * @returns {JSX.Element} The rendered component with title and optional subtitle.
  */
-const PageTitle = ({ title, subtitle = null }) => {
+const PageTitle = ({ titleKey, subtitleKey = null }) => {
+  // Initialize the translation function
+  const { t } = useTranslation();
+
   return (
     <>
       <Grid
@@ -27,14 +34,14 @@ const PageTitle = ({ title, subtitle = null }) => {
         }}
       >
         {/* Render the main title */}
-        <Typography variant="h2" gutterBottom sx={{fontSize: 50}}>
-          {title}
+        <Typography variant="h2" gutterBottom sx={{ fontSize: 50 }}>
+          {t(titleKey)}
         </Typography>
 
         {/* Conditionally render the subtitle if it's provided */}
-        {subtitle && (
+        {subtitleKey && (
           <Typography variant="h5" gutterBottom>
-            {subtitle}
+            {t(subtitleKey)}
           </Typography>
         )}
 
@@ -47,8 +54,8 @@ const PageTitle = ({ title, subtitle = null }) => {
 
 // Props validation for the component using PropTypes
 PageTitle.propTypes = {
-  title: PropTypes.string.isRequired,
-  subtitle: PropTypes.string,
+  titleKey: PropTypes.string.isRequired,
+  subtitleKey: PropTypes.string,
 };
 
 export default PageTitle;
