0aced8afbaa661f071909e756d7e7b3e1303fd7f
Kailash Kidd <kiddkailash@gmail.com>
2025-01-07 00:08:56 +1000

    Password visibility + aesthetics

--- src/webpages/Login.jsx
+++ src/webpages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
 import axios from "axios";
-import { useNavigate, useSearchParams, Link as RouterLink } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { UserContext } from "../contexts/UserContext";
 import { SnackbarContext } from "../contexts/SnackbarContext";
 
@@ -12,11 +12,12 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Checkbox from "@mui/material/Checkbox";
 import FormControlLabel from "@mui/material/FormControlLabel";
-import Grid from "@mui/material/Grid";
-import Radio from "@mui/material/Radio";
-import RadioGroup from "@mui/material/RadioGroup";
-import FormControl from "@mui/material/FormControl";
+import Grid from "@mui/material/Grid2";
 import Link from "@mui/material/Link";
+import IconButton from "@mui/material/IconButton";
+import InputAdornment from "@mui/material/InputAdornment";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
 import { useTranslation, Trans } from "react-i18next";
 
@@ -26,16 +27,18 @@ const LoginPage = () => {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
-  const [company, setCompany] = useState("");
   const [tosChecked, setTosChecked] = useState(false);
   const [errors, setErrors] = useState({});
   const [authMode, setAuthMode] = useState(initialMode);
   const [loading, setLoading] = useState(false);
 
-  const { resetUserContext, setUser, setIsLoggedIn, isLoggedIn } = useContext(UserContext);
+  const { resetUserContext, setUser, setIsLoggedIn, isLoggedIn } =
+    useContext(UserContext);
   const { showSnackbar } = useContext(SnackbarContext);
 
   const navigate = useNavigate();
@@ -64,7 +67,6 @@ const LoginPage = () => {
     setConfirmPassword("");
     setFirstName("");
     setLastName("");
-    setCompany("");
     setTosChecked(false);
     setErrors({});
   };
@@ -86,7 +88,7 @@ const LoginPage = () => {
     setErrors(newErrors);
 
     if (Object.keys(newErrors).length > 0) {
-      showSnackbar(t("please_fill_in_all_required_fields"), "error");
+      showSnackbar(t("please_fill_in_all_required_fields"), "warning");
       return false;
     }
 
@@ -96,7 +98,7 @@ const LoginPage = () => {
   const validateTOS = () => {
     if (authMode === "create" && !tosChecked) {
       setErrors((prev) => ({ ...prev, tos: true }));
-      showSnackbar(t("must_agree_to_terms"), "error");
+      showSnackbar(t("must_agree_to_terms"), "warning");
       return false;
     }
     return true;
@@ -112,7 +114,7 @@ const LoginPage = () => {
     }
 
     if (authMode === "create" && password !== confirmPassword) {
-      showSnackbar(t("passwords_do_not_match"), "error");
+      showSnackbar(t("passwords_do_not_match"), "warning");
       setErrors((prev) => ({ ...prev, password: true, confirmPassword: true }));
       setLoading(false);
       return;
@@ -128,14 +130,11 @@ const LoginPage = () => {
 
     const payload =
       authMode === "create"
-        ? { email, password, firstName, lastName, company: company || null }
+        ? { email, password, firstName, lastName }
         : { email, password };
 
     try {
-      const response = await axios.post(
-        `${BACKEND}${endpoint}`,
-        payload
-      );
+      const response = await axios.post(`${BACKEND}${endpoint}`, payload);
 
       const { token, user } = response.data;
 
@@ -154,7 +153,10 @@ const LoginPage = () => {
       );
     } catch (err) {
       console.error(err);
-      showSnackbar(err.response?.data?.error || t("error_occurred_try_again"), "error");
+      showSnackbar(
+        err.response?.data?.error || t("error_occurred_try_again"),
+        "error"
+      );
     } finally {
       setLoading(false);
     }
@@ -167,9 +169,6 @@ const LoginPage = () => {
         break;
       case "lastName":
         setLastName(value);
-        break;
-      case "company":
-        setCompany(value);
         break;
       case "email":
         setEmail(value);
@@ -193,9 +192,22 @@ const LoginPage = () => {
     }
   };
 
+  const handleClickShowPassword = () => {
+    setShowPassword((prev) => !prev);
+  };
+
+  const handleClickShowConfirmPassword = () => {
+    setShowConfirmPassword((prev) => !prev);
+  };
+
+  // Prevent the input losing focus when clicking the icon
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <Container
-      maxWidth="sm"
+      maxWidth="xs"
       sx={{
         border: "1px solid #e0e0e0",
         borderRadius: 2,
@@ -203,160 +215,225 @@ const LoginPage = () => {
         mt: 3,
       }}
     >
-      <Box textAlign="center" sx={{ transition: "all 0.3s ease-in-out", mb: 2 }}>
-        <FormControl component="fieldset" sx={{ mb: 1 }}>
-          <RadioGroup
-            row
-            aria-label="auth mode"
-            name="auth-mode"
-            value={authMode}
-            onChange={handleAuthModeChange}
-          >
-            <FormControlLabel
-              value="login"
-              control={<Radio />}
-              label={t("login")}
-            />
-            <FormControlLabel
-              value="create"
-              control={<Radio />}
-              label={t("create_account")}
-            />
-          </RadioGroup>
-        </FormControl>
-
+      <Box
+        textAlign="center"
+        sx={{ transition: "all 0.3s ease-in-out", mb: 2 }}
+      >
         <Typography variant="h4" color="primary">
-          Study Buddy
+          StudyBuddy.ai
         </Typography>
-        <Typography variant="h5" color="textPrimary" sx={{ mt: 1, fontWeight: "bold" }}>
+        <Typography
+          variant="h5"
+          color="textPrimary"
+          sx={{ mt: 1, pr: 7, pl: 7, fontWeight: 600 }}
+        >
           {authMode === "create"
             ? t("create_study_cards_from_resource")
             : t("login_to_your_account")}
         </Typography>
-        <Typography variant="body1" color="textSecondary" sx={{ mt: 1 }}>
-          {authMode === "create"
-            ? t("create_mode_description")
-            : t("login_mode_description")}
+        <Typography variant="subtitle1" color="textSecondary" sx={{ mt: 1 }}>
+          <Trans
+            i18nKey={
+              authMode === "create"
+                ? "create_mode_description"
+                : "login_mode_description"
+            }
+            components={[
+              <Link
+                key="0" // Each component must have a unique key
+                component="span"
+                variant="body1"
+                onClick={(e) => {
+                  e.preventDefault(); // Prevent default button behavior
+                  setAuthMode(authMode === "create" ? "login" : "create");
+                }}
+              />,
+            ]}
+          />
         </Typography>
       </Box>
 
       <Box component="form" onSubmit={handleSubmit}>
-        {authMode === "create" && (
-          <>
-            <Grid container spacing={2} sx={{ mb: 1.5 }}>
-              <Grid item xs={12} sm={6}>
+        <Grid container rowSpacing={1.5} columnSpacing={1}>
+          {authMode === "create" && (
+            <>
+              <Grid size={{ xs: 12, sm: 6 }}>
                 <TextField
                   fullWidth
                   label={t("first_name")}
                   variant="outlined"
                   value={firstName}
-                  onChange={(e) => handleFieldChange("firstName", e.target.value)}
+                  onChange={(e) =>
+                    handleFieldChange("firstName", e.target.value)
+                  }
                   error={!!errors.firstName}
                 />
               </Grid>
-              <Grid item xs={12} sm={6}>
+              <Grid size={{ xs: 12, sm: 6 }}>
                 <TextField
                   fullWidth
                   label={t("last_name")}
                   variant="outlined"
                   value={lastName}
-                  onChange={(e) => handleFieldChange("lastName", e.target.value)}
+                  onChange={(e) =>
+                    handleFieldChange("lastName", e.target.value)
+                  }
                   error={!!errors.lastName}
                 />
               </Grid>
-            </Grid>
+            </>
+          )}
+
+          <Grid size={12}>
             <TextField
               fullWidth
-              label={t("company")}
+              label={t("email")}
+              type="email"
               variant="outlined"
-              sx={{ mb: 1.5 }}
-              value={company}
-              onChange={(e) => handleFieldChange("company", e.target.value)}
+              value={email}
+              onChange={(e) => handleFieldChange("email", e.target.value)}
+              error={!!errors.email}
             />
-          </>
-        )}
-
-        <TextField
-          fullWidth
-          label={t("email")}
-          type="email"
-          variant="outlined"
-          sx={{ mb: 1.5 }}
-          value={email}
-          onChange={(e) => handleFieldChange("email", e.target.value)}
-          error={!!errors.email}
-        />
-        <TextField
-          fullWidth
-          label={t("password")}
-          type="password"
-          variant="outlined"
-          sx={{ mb: 1.5 }}
-          value={password}
-          onChange={(e) => handleFieldChange("password", e.target.value)}
-          error={!!errors.password}
-        />
-
-        {authMode === "create" && (
-          <TextField
-            fullWidth
-            label={t("re_enter_password")}
-            type="password"
-            variant="outlined"
-            sx={{ mb: 1.5 }}
-            value={confirmPassword}
-            onChange={(e) => handleFieldChange("confirmPassword", e.target.value)}
-            error={!!errors.confirmPassword}
-          />
-        )}
-
-        {authMode === "create" && (
-          <FormControlLabel
-            control={
-              <Checkbox
-                color="primary"
-                checked={tosChecked}
-                onChange={(e) => {
-                  setTosChecked(e.target.checked);
-                  if (errors.tos) {
-                    setErrors((prev) => {
-                      const newErrors = { ...prev };
-                      delete newErrors.tos;
-                      return newErrors;
-                    });
+          </Grid>
+          <Grid size={12}>
+            <TextField
+              label={t("password")}
+              fullWidth
+              type={showPassword ? "text" : "password"}
+              variant="outlined"
+              value={password}
+              onChange={(e) => handleFieldChange("password", e.target.value)}
+              error={!!errors.password}
+              slotProps={{
+                input: {
+                  endAdornment: (
+                    <InputAdornment position="end" sx={{ mr: 1 }}>
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                        sx={{
+                          fontSize: 'small',
+                          opacity: 0.5, // Reduce visibility slightly
+                          "&:hover": {
+                            opacity: 0.7, // Fully visible on hover
+                          },
+                          color: "text.secondary", // Use muted text color
+                        }}
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                },
+              }}
+            />
+          </Grid>
+          {authMode === "create" && (
+            <>
+              <Grid size={12}>
+                <TextField
+                  fullWidth
+                  label={t("re_enter_password")}
+                  type={showConfirmPassword ? "text" : "password"}
+                  variant="outlined"
+                  value={confirmPassword}
+                  onChange={(e) =>
+                    handleFieldChange("confirmPassword", e.target.value)
                   }
-                }}
-              />
-            }
-            label={
-              // Use the Trans component with placeholders replaced by links.
-              <Trans
-                i18nKey="agree_to_terms"
-                components={[
-                  <Link component={RouterLink} to="/terms" key="0" />,
-                  <Link component={RouterLink} to="/privacy" key="1" />,
-                ]}
-              />
-            }
-            sx={{ mb: 1.5 }}
-          />
-        )}
-
-        <Button
-          fullWidth
-          variant="contained"
-          color="primary"
-          type="submit"
-          disabled={loading}
-        >
-          {loading
-            ? authMode === "create"
-              ? t("creating_account")
-              : t("logging_in")
-            : authMode === "create"
-            ? t("create_your_clipcard_account")
-            : t("login")}
-        </Button>
+                  error={!!errors.confirmPassword}
+                  slotProps={{
+                    input: {
+                      endAdornment: (
+                        <InputAdornment position="end" sx={{ mr: 1 }}>
+                          <IconButton
+                            aria-label="toggle confirm password visibility"
+                            onClick={handleClickShowConfirmPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            edge="end"
+                            sx={{
+                              opacity: 0.5, // Reduce visibility slightly
+                              "&:hover": {
+                                opacity: 0.7, // Fully visible on hover
+                              },
+                              color: "text.secondary", // Use muted text color
+                            }}
+                          >
+                            {showConfirmPassword ? (
+                              <VisibilityOff />
+                            ) : (
+                              <Visibility />
+                            )}
+                          </IconButton>
+                        </InputAdornment>
+                      ),
+                    },
+                  }}
+                />
+              </Grid>
+              <Grid size={12} sx={{ textAlign: "left" }}>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      color="primary"
+                      checked={tosChecked}
+                      onChange={(e) => {
+                        setTosChecked(e.target.checked);
+                        if (errors.tos) {
+                          setErrors((prev) => {
+                            const newErrors = { ...prev };
+                            delete newErrors.tos;
+                            return newErrors;
+                          });
+                        }
+                      }}
+                    />
+                  }
+                  label={
+                    // Use the Trans component with placeholders replaced by links.
+                    <Trans
+                      i18nKey="agree_to_terms"
+                      components={[
+                        <Link
+                          component="span"
+                          onClick={() => {
+                            navigate("/terms");
+                          }}
+                          key="0"
+                        />,
+                        <Link
+                          component="span"
+                          onClick={() => {
+                            navigate("/privacy");
+                          }}
+                          key="1"
+                        />,
+                      ]}
+                    />
+                  }
+                />
+              </Grid>
+            </>
+          )}
+          <Grid size={12}>
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              type="submit"
+              disabled={loading}
+            >
+              {loading
+                ? authMode === "create"
+                  ? t("creating_account")
+                  : t("logging_in")
+                : authMode === "create"
+                ? t("create_your_clipcard_account")
+                : t("login")}
+            </Button>
+          </Grid>
+        </Grid>
       </Box>
     </Container>
   );
