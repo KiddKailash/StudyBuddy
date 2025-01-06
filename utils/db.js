a649db4f5a02dca8d57b1b6de932f2ff9a767dbf
Kailash Kidd <kiddkailash@gmail.com>
2025-01-06 16:01:27 +1000

    Changed DB provider MongoDB -> Digital Ocean via MongoBD

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
-const uri = process.env.MONGODB_URI;
+const uri = process.env.DATABASE_URL;
 
 const client = new MongoClient(uri, {
   tls: true,
