4579ec2c60383d45c5fba6ff5d8d2b804d869422
Kailash Kidd <kiddkailash@gmail.com>
2025-01-20 23:00:36 +0700

    Restuructured folders

--- environment/start-cron.sh
+++ environment/start-cron.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # 1. Export all runtime environment variables so cron can see them
-#    - Exclude "no_proxy" if you like (some distros do by default)
 printenv | grep -v "no_proxy" >> /etc/environment
 
 # 2. Start cron in the background
