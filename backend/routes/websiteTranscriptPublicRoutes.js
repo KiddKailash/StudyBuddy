49c10c577fa9dabd35ca4a05a6bde0b2f2940bb2
Kailash Kidd <kiddkailash@gmail.com>
2025-05-14 10:29:48 +1000

    Wiped Repo

--- backend/routes/websiteTranscriptPublicRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const websiteTranscriptController = require("../controllers/websiteTranscriptController");
-
-// Public route for scraping a website
-router.get("/", websiteTranscriptController.fetchWebsiteTranscript);
-
-module.exports = router;
--- backend/routes/websiteTranscriptRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const websiteTranscriptController = require("../controllers/websiteTranscriptController");
-const authMiddleware = require("../middleware/authMiddleware");
-
-// Protected route for scraping a website
-router.get("/", authMiddleware, websiteTranscriptController.fetchWebsiteTranscript);
-
-module.exports = router;
